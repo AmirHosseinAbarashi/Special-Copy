f95c108095bbb24defd33380fcbd377c31ac8395
AmirHosseinAbarashi <ah.abarashi@gmail.com>
2022-02-08 23:48:29 +0330

    add CopyDirectory Class

---
+++ src/CopyDirectory.java
@@ -0,0 +1,69 @@
+import java.io.*;
+
+public class CopyDirectory {
+
+    private String in , out;
+    private File fileIn , fileOut;            // fileOut -> out
+
+
+    public CopyDirectory(String in , String out){
+        this.in = in;
+        this.out = out;
+
+        StringBuilder inSBLD = new StringBuilder(in);
+        StringBuilder outSBLD = new StringBuilder(out);
+
+        for (int i = 0; i < inSBLD.length(); i++)                         // conversion to a standard path (in)
+            if (inSBLD.charAt(i) == '\\')
+                inSBLD.setCharAt(i , '/');
+
+        for (int i = 0; i < outSBLD.length(); i++)                       // conversion to a standard path (out)
+            if (outSBLD.charAt(i) == '\\')
+                outSBLD.setCharAt(i , '/');
+
+        fileIn = new File(String.valueOf(inSBLD));
+        fileOut = new File(String.valueOf(outSBLD));
+
+    }
+
+    public void copy() throws IOException {
+        copyDirectoryCompatibityMode(fileIn , fileOut);
+    }
+
+    private void copyDirectoryCompatibityMode(File source, File destination) throws IOException
+    {
+        if (source.isDirectory()) {                             // source is a directory
+            copyDirectory(source, destination);
+        } else {                                                // source is a file
+            copyFile(source, destination);
+        }
+    }
+
+    private void copyDirectory(File sourceDirectory, File destinationDirectory) throws IOException
+    {
+        if (!destinationDirectory.exists())                    // if path end not exist -> will build it
+            destinationDirectory.mkdir();
+
+        for (String f : sourceDirectory.list())
+        {
+            if (new  File(sourceDirectory , f).isDirectory())       // if f is a directory
+                copyDirectoryCompatibityMode(new File(sourceDirectory, f), destinationDirectory);
+            else                                                   // if f is a file
+                copyDirectoryCompatibityMode(new File(sourceDirectory, f), new File(destinationDirectory , f));
+        }
+    }
+
+    private void copyFile(File sourceFile, File destinationFile)      // copy file
+            throws IOException {
+        try (InputStream in = new FileInputStream(sourceFile);
+             OutputStream out = new FileOutputStream(destinationFile))
+        {
+            byte[] buf = new byte[1024];
+            int length;
+            while ((length = in.read(buf)) > 0) {
+                out.write(buf, 0, length);
+            }
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -8,63 +8,8 @@ class Main
 
         Scanner scanner = new Scanner(System.in);
 
-        StringBuilder in = new StringBuilder(scanner.nextLine());     // get path of begin
-        StringBuilder out = new StringBuilder(scanner.nextLine());    // get path of end
-
-        for (int i = 0; i < in.length(); i++)                         // conversion to a standard path (in)
-            if (in.charAt(i) == '\\')
-                in.setCharAt(i , '/');
-
-        for (int i = 0; i < out.length(); i++)                       // conversion to a standard path (out)
-            if (out.charAt(i) == '\\')
-                out.setCharAt(i , '/');
-
-
-        File fileIn = new File(String.valueOf(in));                 // fileIn -> in
-        File fileOut = new File(String.valueOf(out));               // fileOut -> out
-
-
-
-        if (fileIn.exists())
-            copyDirectoryCompatibityMode(fileIn , fileOut);        // if fileIn is exist
-        else
-            System.out.println("File not exist.");                 // if fileIn not exist
-
+        CopyDirectory copyDirectory = new CopyDirectory(scanner.nextLine() , scanner.nextLine());
+        copyDirectory.copy();
     }
 
-    static void copyDirectoryCompatibityMode(File source, File destination) throws IOException
-    {
-        if (source.isDirectory()) {                             // source is a directory
-            copyDirectory(source, destination);
-        } else {                                                // source is a file
-            copyFile(source, destination);
-        }
-    }
-
-    static void copyDirectory(File sourceDirectory, File destinationDirectory) throws IOException
-    {
-        if (!destinationDirectory.exists())                    // if path end not exist -> will build it
-            destinationDirectory.mkdir();
-
-        for (String f : sourceDirectory.list())
-        {
-            if (new  File(sourceDirectory , f).isDirectory())       // if f is a directory
-                copyDirectoryCompatibityMode(new File(sourceDirectory, f), destinationDirectory);
-            else                                                   // if f is a file
-                copyDirectoryCompatibityMode(new File(sourceDirectory, f), new File(destinationDirectory , f));
-        }
-    }
-
-    static void copyFile(File sourceFile, File destinationFile)      // copy file
-            throws IOException {
-        try (InputStream in = new FileInputStream(sourceFile);
-             OutputStream out = new FileOutputStream(destinationFile))
-        {
-            byte[] buf = new byte[1024];
-            int length;
-            while ((length = in.read(buf)) > 0) {
-                out.write(buf, 0, length);
-            }
-        }
-    }
 }
