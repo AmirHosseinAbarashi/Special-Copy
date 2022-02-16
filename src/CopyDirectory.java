af932e1e0c8759f139311208da0efd9a8fc51243
AmirHosseinAbarashi <ah.abarashi@gmail.com>
2022-02-16 17:43:54 +0330

    Add logging and Bug fixes

--- src/CopyDirectory.java
+++ src/CopyDirectory.java
@@ -1,11 +1,15 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.logging.Logger;
 
 public class CopyDirectory {
 
-    private String in, out;
-    private File fileIn, fileOut;            // fileOut -> out
+    private final File fileIn;
+    private final File fileOut;
+    private final Logger logger = Logger.getLogger("copy");
+    private final String in;
+    private final String out;
 
 
     public CopyDirectory(String in, String out) {
@@ -15,11 +19,11 @@ public class CopyDirectory {
         StringBuilder inSBLD = new StringBuilder(in);
         StringBuilder outSBLD = new StringBuilder(out);
 
-        for (int i = 0; i < inSBLD.length(); i++)                         // conversion to a standard path (in)
+        for (int i = 0; i < inSBLD.length(); i++)                         // conversion to a standard path (in) [  \  ->  /  ]
             if (inSBLD.charAt(i) == '\\')
                 inSBLD.setCharAt(i, '/');
 
-        for (int i = 0; i < outSBLD.length(); i++)                       // conversion to a standard path (out)
+        for (int i = 0; i < outSBLD.length(); i++)                       // conversion to a standard path (out) [  \  ->  /  ]
             if (outSBLD.charAt(i) == '\\')
                 outSBLD.setCharAt(i, '/');
 
@@ -29,10 +33,12 @@ public class CopyDirectory {
     }
 
     public void copy() throws IOException {
+        logger.info("The copy order was registered by the user.");
         copyDirectoryCompatibityMode(fileIn, fileOut);
     }
 
     private void copyDirectoryCompatibityMode(File source, File destination) throws IOException {
+        logger.info("copyDirectoryCompatibityMode:" + source.toString());
         if (source.isDirectory()) {                             // source is a directory
             copyDirectory(source, destination);
         } else {                                                // source is a file
@@ -45,7 +51,14 @@ public class CopyDirectory {
             destinationDirectory.mkdir();
 
         for (String f : sourceDirectory.list()) {
-            if (new File(sourceDirectory, f).isDirectory())       // if f is a directory
+            if (new File(destinationDirectory, f).isFile() && new File(destinationDirectory, f).exists()) {       // (f is file?) and (f is exist?)
+                logger.info("file " + f + " in " + out + " is exist.");
+                if (new File(destinationDirectory, sourceDirectory.hashCode() + "_Copy" + f).exists()) {    // f was copied!
+                    logger.info(f + " was copied. Delete and Copy again.");
+                    Files.delete(new File(destinationDirectory, sourceDirectory.hashCode() + "_Copy" + f).toPath());
+                }
+                copyDirectoryCompatibityMode(new File(sourceDirectory, f), new File(destinationDirectory, sourceDirectory.hashCode() + "_Copy" + f));
+            } else if (new File(sourceDirectory, f).isDirectory())       // if f is a directory
                 copyDirectoryCompatibityMode(new File(sourceDirectory, f), destinationDirectory);
             else                                                   // if f is a file
                 copyDirectoryCompatibityMode(new File(sourceDirectory, f), new File(destinationDirectory, f));
