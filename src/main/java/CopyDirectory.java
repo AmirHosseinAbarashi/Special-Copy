0fd4ed91b1a1c1f48d33140a2fdd0136d23bbd55
AmirHosseinAbarashi <ah.abarashi@gmail.com>
2022-03-16 11:40:31 +0330

    Using ExecutorService and Runnable

--- src/main/java/CopyDirectory.java
+++ src/main/java/CopyDirectory.java
@@ -1,75 +1,26 @@
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Objects;
-import java.util.logging.Logger;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class CopyDirectory {
 
-    private final Logger logger = Logger.getLogger(getClass().getName());
     private final File fileIn;
     private final File fileOut;
-
+    ExecutorService es = Executors.newFixedThreadPool(4);
 
     public CopyDirectory(String in, String out) {
         fileIn = new File(String.valueOf(in));
         fileOut = new File(String.valueOf(out));
     }
 
-    public void copy() {
+    public void copy() throws InterruptedException {
         for (String f : Objects.requireNonNull(fileIn.list())) {
-            Thread t = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        copyDirectoryCompatibilityMode(new File(fileIn , f) , fileOut);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                private void copyDirectoryCompatibilityMode(File source, File destination) throws IOException {
-                    logger.info("copying " + source + " to " + destination);
-                    if (source.isDirectory()) {                             // source is a directory
-                        copyDirectory(source, destination);
-                    } else {                                                // source is a file
-                        Files.copy(source.toPath(), destination.toPath());
-                    }
-                }
-
-
-            });
-            t.start();
+            Runnable runnable = new CopyDirectoryThread(fileIn, fileOut, f);
+            es.execute(runnable);
         }
-    }
-
-    private void copyDirectoryCompatibilityMode(File source, File destination) throws IOException {
-        logger.info("copying " + source + " to " + destination);
-        if (source.isDirectory()) {                             // source is a directory
-            copyDirectory(source, destination);
-        } else {                                                // source is a file
-            Files.copy(source.toPath(), destination.toPath());
-        }
-    }
-
-    private void copyDirectory(File sourceDirectory, File destinationDirectory) throws IOException {
-        if (!destinationDirectory.exists())                    // if path end not exist -> will build it
-            if (!destinationDirectory.mkdir())
-                throw new IOException("Could not created directory");
-
-        for (String f : Objects.requireNonNull(sourceDirectory.list())) {
-            if (new File(destinationDirectory, f).isFile() && new File(destinationDirectory, f).exists()) {       // (f is file?) and (f is exist?)
-                logger.info("file " + f + " in " + fileOut.toString() + " is exist.");
-                File file = new File(destinationDirectory, sourceDirectory.hashCode() + "_Copy" + f);
-                if (file.exists()) {
-                    logger.info(f + " was copied.");
-                    Files.delete(file.toPath());
-                }
-                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), file);
-            } else if (new File(sourceDirectory, f).isDirectory())       // if f is a directory
-                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), destinationDirectory);
-            else                                                   // if f is a file
-                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), new File(destinationDirectory, f));
-        }
+        es.shutdown();
+        es.awaitTermination(30000, TimeUnit.MILLISECONDS);
     }
 }
--- src/main/java/CopyDirectoryThread.java
+++ src/main/java/CopyDirectoryThread.java
@@ -1,2 +1,67 @@
-public class CopyDirectoryThread {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class CopyDirectoryThread implements Runnable {
+
+    private final Logger logger = Logger.getLogger(getClass().getName());
+    private final File fileIn;
+    private final File fileOut;
+    private final String f;
+
+    public CopyDirectoryThread(File fileIn, File fileOut, String f) {
+        this.fileIn = fileIn;
+        this.fileOut = fileOut;
+        this.f = f;
+    }
+
+    @Override
+    public void run() {
+        if (new File(fileIn, f).isDirectory()) {
+            try {
+                copyDirectoryCompatibilityMode(new File(fileIn, f), fileOut);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Files.copy(new File(fileIn, f).toPath(), new File(fileOut, f).toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void copyDirectoryCompatibilityMode(File source, File destination) throws IOException {
+        logger.info("copying " + source + " to " + destination);
+        if (source.isDirectory()) {                             // source is a directory
+            copyDirectory(source, destination);
+        } else {                                                // source is a file
+            Files.copy(source.toPath(), destination.toPath());
+        }
+    }
+
+    private void copyDirectory(File sourceDirectory, File destinationDirectory) throws IOException {
+        if (!destinationDirectory.exists())                    // if path end not exist -> will build it
+            if (!destinationDirectory.mkdir())
+                throw new IOException("Could not created directory");
+
+        for (String f : Objects.requireNonNull(sourceDirectory.list())) {
+            if (new File(destinationDirectory, f).isFile() && new File(destinationDirectory, f).exists()) {       // (f is file?) and (f is exist?)
+                logger.info("file " + f + " in " + fileOut.toString() + " is exist.");
+                File file = new File(destinationDirectory, sourceDirectory.hashCode() + "_Copy" + f);
+                if (file.exists()) {
+                    logger.info(f + " was copied.");
+                    Files.delete(file.toPath());
+                }
+                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), file);
+            } else if (new File(sourceDirectory, f).isDirectory())       // if f is a directory
+                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), destinationDirectory);
+            else                                                   // if f is a file
+                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), new File(destinationDirectory, f));
+        }
+    }
+
 }
