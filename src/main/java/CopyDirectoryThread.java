e3495aabf5bcefcfc0bc31a24c658ca0e45d7721
AmirHosseinAbarashi <ah.abarashi@gmail.com>
2022-03-23 01:09:34 +0430

    Multilevel editing

--- src/main/java/CopyDirectoryThread.java
+++ src/main/java/CopyDirectoryThread.java
@@ -1,27 +1,28 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
-public class CopyDirectoryThread implements Runnable {
+public class CopyDirectoryThread implements Callable<Void> {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
-    private final File fileIn;
-    private final File fileOut;
+    private final String fileIn;
+    private final String fileOut;
     private final String f;
 
-    public CopyDirectoryThread(File fileIn, File fileOut, String f) {
+    public CopyDirectoryThread(String fileIn, String fileOut, String f) {
         this.fileIn = fileIn;
         this.fileOut = fileOut;
         this.f = f;
     }
 
     @Override
-    public void run() {
+    public Void call() throws Exception {
         if (new File(fileIn, f).isDirectory()) {
             try {
-                copyDirectoryCompatibilityMode(new File(fileIn, f), fileOut);
+                copyDirectoryCompatibilityMode(new File(fileIn, f), new File(fileOut));
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -32,36 +33,12 @@ public class CopyDirectoryThread implements Runnable {
                 e.printStackTrace();
             }
         }
+        return null;
     }
 
-    private void copyDirectoryCompatibilityMode(File source, File destination) throws IOException {
+    private void copyDirectoryCompatibilityMode(File source, File destination) throws IOException, ExecutionException, InterruptedException {
         logger.info("copying " + source + " to " + destination);
-        if (source.isDirectory()) {                             // source is a directory
-            copyDirectory(source, destination);
-        } else {                                                // source is a file
-            Files.copy(source.toPath(), destination.toPath());
-        }
+        CopyDirectory cd = new CopyDirectory();
+        cd.copy(source.toPath().toString(), destination.toPath().toString());
     }
-
-    private void copyDirectory(File sourceDirectory, File destinationDirectory) throws IOException {
-        if (!destinationDirectory.exists())                    // if path end not exist -> will build it
-            if (!destinationDirectory.mkdir())
-                throw new IOException("Could not created directory");
-
-        for (String f : Objects.requireNonNull(sourceDirectory.list())) {
-            if (new File(destinationDirectory, f).isFile() && new File(destinationDirectory, f).exists()) {       // (f is file?) and (f is exist?)
-                logger.info("file " + f + " in " + fileOut.toString() + " is exist.");
-                File file = new File(destinationDirectory, sourceDirectory.hashCode() + "_Copy" + f);
-                if (file.exists()) {
-                    logger.info(f + " was copied.");
-                    Files.delete(file.toPath());
-                }
-                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), file);
-            } else if (new File(sourceDirectory, f).isDirectory())       // if f is a directory
-                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), destinationDirectory);
-            else                                                   // if f is a file
-                copyDirectoryCompatibilityMode(new File(sourceDirectory, f), new File(destinationDirectory, f));
-        }
-    }
-
 }
